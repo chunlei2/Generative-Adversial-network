a4b7961de6b807dadd0f004456d4f2f7746fa31c
chunlei2 <chunleiliu1995@gmail.com>
2018-12-04 20:05:53 -0600

    First commit

---
+++ visualization.py
@@ -0,0 +1,316 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from torch import autograd
+import utils
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+import time
+import warnings
+warnings.filterwarnings("ignore")
+
+batch_size = 100
+def plot(samples):
+    fig = plt.figure(figsize=(10, 10))
+    gs = gridspec.GridSpec(10, 10)
+    gs.update(wspace=0.02, hspace=0.02)
+
+    for i, sample in enumerate(samples):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample)
+    return fig
+transform_test = transforms.Compose([
+    transforms.CenterCrop(32),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+testset = torchvision.datasets.CIFAR10(root='./', train=False, download=False, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=8)
+testloader = enumerate(testloader)
+
+model = torch.load('cifar10.model')
+model.cuda()
+model.eval()
+
+
+for batch_idx, (X_batch, Y_batch) in testloader:
+    X_batch = Variable(X_batch,requires_grad=True).cuda()
+    Y_batch_alternate = (Y_batch + 1)%10
+    Y_batch_alternate = Variable(Y_batch_alternate).cuda()
+    Y_batch = Variable(Y_batch).cuda()
+    break
+
+## save real images
+samples = X_batch.data.cpu().numpy()
+samples += 1.0
+samples /= 2.0
+samples = samples.transpose(0,2,3,1)
+
+fig = plot(samples[0:100])
+plt.savefig('visualization/real_images.png', bbox_inches='tight')
+plt.close(fig)
+
+output = model(X_batch)
+output = output[1]
+prediction = output.data.max(1)[1] # first column has actual prob.
+accuracy = ( float( prediction.eq(Y_batch.data).sum() ) /float(batch_size))*100.0
+print(accuracy)
+
+## slightly jitter all input images
+criterion = nn.CrossEntropyLoss(reduce=False)
+loss = criterion(output, Y_batch_alternate)
+
+gradients = torch.autograd.grad(outputs=loss, inputs=X_batch,
+                          grad_outputs=torch.ones(loss.size()).cuda(),
+                          create_graph=True, retain_graph=False, only_inputs=True)[0]
+
+gradient_image = gradients.data.cpu().numpy()
+gradient_image = (gradient_image - np.min(gradient_image))/(np.max(gradient_image)-np.min(gradient_image))
+gradient_image = gradient_image.transpose(0,2,3,1)
+fig = plot(gradient_image[0:100])
+plt.savefig('visualization/gradient_image.png', bbox_inches='tight')
+plt.close(fig)
+
+# jitter input image
+gradients[gradients>0.0] = 1.0
+gradients[gradients<0.0] = -1.0
+
+gain = 8.0
+X_batch_modified = X_batch - gain*0.007843137*gradients
+X_batch_modified[X_batch_modified>1.0] = 1.0
+X_batch_modified[X_batch_modified<-1.0] = -1.0
+
+## evaluate new fake images
+output = model(X_batch_modified)
+output = output[1]
+prediction = output.data.max(1)[1] # first column has actual prob.
+accuracy = ( float( prediction.eq(Y_batch.data).sum() ) /float(batch_size))*100.0
+print(accuracy)
+
+## save fake images
+samples = X_batch_modified.data.cpu().numpy()
+samples += 1.0
+samples /= 2.0
+samples = samples.transpose(0,2,3,1)
+
+fig = plot(samples[0:100])
+plt.savefig('visualization/jittered_images.png', bbox_inches='tight')
+plt.close(fig)
+
+# Synthetic Images Maximizing Classification Output
+X = X_batch.mean(dim=0)
+X = X.repeat(10,1,1,1)
+
+Y = torch.arange(10).type(torch.int64)
+Y = Variable(Y).cuda()
+
+lr = 0.1
+weight_decay = 0.001
+for i in range(200):
+    output = model(X)
+    output = output[1] 
+    loss = -output[torch.arange(10).type(torch.int64),torch.arange(10).type(torch.int64)]
+    gradients = torch.autograd.grad(outputs=loss, inputs=X,
+                              grad_outputs=torch.ones(loss.size()).cuda(),
+                              create_graph=True, retain_graph=False, only_inputs=True)[0]
+
+    prediction = output.data.max(1)[1] # first column has actual prob.
+    accuracy = ( float( prediction.eq(Y.data).sum() ) /float(10.0))*100.0
+    print(i,accuracy,-loss)
+
+    X = X - lr*gradients.data - weight_decay*X.data*torch.abs(X.data)
+    X[X>1.0] = 1.0
+    X[X<-1.0] = -1.0
+
+## save new images
+samples = X.data.cpu().numpy()
+samples += 1.0
+samples /= 2.0
+samples = samples.transpose(0,2,3,1)
+
+fig = plot(samples)
+plt.savefig('visualization/max_class_no_G.png', bbox_inches='tight')
+plt.close(fig)
+
+model = torch.load('discriminator.model')
+
+# Synthetic Images Maximizing Classification Output
+X = X_batch.mean(dim=0)
+X = X.repeat(10,1,1,1)
+
+Y = torch.arange(10).type(torch.int64)
+Y = Variable(Y).cuda()
+
+lr = 0.1
+weight_decay = 0.001
+for i in range(200):
+    output = model(X)
+    output = output[1] 
+    loss = -output[torch.arange(10).type(torch.int64),torch.arange(10).type(torch.int64)]
+    gradients = torch.autograd.grad(outputs=loss, inputs=X,
+                              grad_outputs=torch.ones(loss.size()).cuda(),
+                              create_graph=True, retain_graph=False, only_inputs=True)[0]
+
+    prediction = output.data.max(1)[1] # first column has actual prob.
+    accuracy = ( float( prediction.eq(Y.data).sum() ) /float(10.0))*100.0
+    print(i,accuracy,-loss)
+
+    X = X - lr*gradients.data - weight_decay*X.data*torch.abs(X.data)
+    X[X>1.0] = 1.0
+    X[X<-1.0] = -1.0
+
+## save new images
+samples = X.data.cpu().numpy()
+samples += 1.0
+samples /= 2.0
+samples = samples.transpose(0,2,3,1)
+
+fig = plot(samples)
+plt.savefig('visualization/max_class_w_G.png', bbox_inches='tight')
+plt.close(fig)
+
+
+model = torch.load('cifar10.model')
+# Synthetic Features Maximizing Features at Various Layers
+
+X = X_batch.mean(dim=0)
+X = X.repeat(batch_size,1,1,1)
+
+Y = torch.arange(batch_size).type(torch.int64)
+Y = Variable(Y).cuda()
+
+lr = 0.1
+weight_decay = 0.001
+
+for i in range(200):
+    output = model(X, 2) #100*196
+
+    loss = -output[torch.arange(batch_size).type(torch.int64),torch.arange(batch_size).type(torch.int64)]
+    gradients = torch.autograd.grad(outputs=loss, inputs=X,
+                              grad_outputs=torch.ones(loss.size()).cuda(),
+                              create_graph=True, retain_graph=False, only_inputs=True)[0]
+
+    prediction = output.data.max(1)[1] # first column has actual prob.
+    accuracy = ( float( prediction.eq(Y.data).sum() ) /float(batch_size))*100.0
+    print(i,accuracy,-loss)
+
+    X = X - lr*gradients.data - weight_decay*X.data*torch.abs(X.data)
+    X[X>1.0] = 1.0
+    X[X<-1.0] = -1.0
+
+## save new images
+samples = X.data.cpu().numpy()
+samples += 1.0
+samples /= 2.0
+samples = samples.transpose(0,2,3,1)
+
+fig = plot(samples[0:100])
+plt.savefig('visualization/max_features_no_G_layer2.png', bbox_inches='tight')
+plt.close(fig)
+
+for i in range(200):
+    output = model(X, 8) #100*196
+
+    loss = -output[torch.arange(batch_size).type(torch.int64),torch.arange(batch_size).type(torch.int64)]
+    gradients = torch.autograd.grad(outputs=loss, inputs=X,
+                              grad_outputs=torch.ones(loss.size()).cuda(),
+                              create_graph=True, retain_graph=False, only_inputs=True)[0]
+
+    prediction = output.data.max(1)[1] # first column has actual prob.
+    accuracy = ( float( prediction.eq(Y.data).sum() ) /float(batch_size))*100.0
+    print(i,accuracy,-loss)
+
+    X = X - lr*gradients.data - weight_decay*X.data*torch.abs(X.data)
+    X[X>1.0] = 1.0
+    X[X<-1.0] = -1.0
+
+## save new images
+samples = X.data.cpu().numpy()
+samples += 1.0
+samples /= 2.0
+samples = samples.transpose(0,2,3,1)
+
+fig = plot(samples[0:100])
+plt.savefig('visualization/max_features_no_G_layer8.png', bbox_inches='tight')
+plt.close(fig)
+
+
+model = torch.load('discriminator.model')
+# Synthetic Features Maximizing Features at Various Layers
+
+X = X_batch.mean(dim=0)
+X = X.repeat(batch_size,1,1,1)
+
+Y = torch.arange(batch_size).type(torch.int64)
+Y = Variable(Y).cuda()
+
+lr = 0.1
+weight_decay = 0.001
+
+for i in range(200):
+    output = model(X, 2) #100*196
+
+    loss = -output[torch.arange(batch_size).type(torch.int64),torch.arange(batch_size).type(torch.int64)]
+    gradients = torch.autograd.grad(outputs=loss, inputs=X,
+                              grad_outputs=torch.ones(loss.size()).cuda(),
+                              create_graph=True, retain_graph=False, only_inputs=True)[0]
+
+    prediction = output.data.max(1)[1] # first column has actual prob.
+    accuracy = ( float( prediction.eq(Y.data).sum() ) /float(batch_size))*100.0
+    print(i,accuracy,-loss)
+
+    X = X - lr*gradients.data - weight_decay*X.data*torch.abs(X.data)
+    X[X>1.0] = 1.0
+    X[X<-1.0] = -1.0
+
+## save new images
+samples = X.data.cpu().numpy()
+samples += 1.0
+samples /= 2.0
+samples = samples.transpose(0,2,3,1)
+
+fig = plot(samples[0:100])
+plt.savefig('visualization/max_features_with_G_layer2.png', bbox_inches='tight')
+plt.close(fig)
+
+
+
+for i in range(200):
+    output = model(X, 8) #100*196
+
+    loss = -output[torch.arange(batch_size).type(torch.int64),torch.arange(batch_size).type(torch.int64)]
+    gradients = torch.autograd.grad(outputs=loss, inputs=X,
+                              grad_outputs=torch.ones(loss.size()).cuda(),
+                              create_graph=True, retain_graph=False, only_inputs=True)[0]
+
+    prediction = output.data.max(1)[1] # first column has actual prob.
+    accuracy = ( float( prediction.eq(Y.data).sum() ) /float(batch_size))*100.0
+    print(i,accuracy,-loss)
+
+    X = X - lr*gradients.data - weight_decay*X.data*torch.abs(X.data)
+    X[X>1.0] = 1.0
+    X[X<-1.0] = -1.0
+
+## save new images
+samples = X.data.cpu().numpy()
+samples += 1.0
+samples /= 2.0
+samples = samples.transpose(0,2,3,1)
+
+fig = plot(samples[0:100])
+plt.savefig('visualization/max_features_with_G_layer8.png', bbox_inches='tight')
+plt.close(fig)
+
